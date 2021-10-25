e3afc1f568edb2af00281e6e133c9a5127c11455
Russell Johnston <rujohns2@cisco.com>
2021-10-25 14:00:43 -0600

    Comment out license

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+/*
 Copyright (c) 2021 Cisco and/or its affiliates.
 
 This software is licensed to you under the terms of the Cisco Sample
@@ -12,6 +13,7 @@ reserved. Unless required by applicable law or agreed to separately in
 writing, software distributed under the License is distributed on an "AS
 IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 or implied.
+*/
 
 locals {
   vapp_properties = {
